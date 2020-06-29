a7b80ccf66959466fc7d338437713ca7edf8b11b
piyush-kgp <piyushsinghkgpian@gmail.com>
2020-06-29 12:28:55 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+
+### Image Classification Server
+
+```
+python3 app.py \
+  --root_dir /Users/piyush/Documents/koala_bear/
+```
+
+
+<img src='Screenshot 2020-06-29 at 12.24.32 PM.png'>
---
+++ app.py
@@ -0,0 +1,44 @@
+
+import argparse
+from flask import Flask
+from flask import request
+from flask import render_template
+from flask import send_from_directory
+import os
+import glob
+
+app = Flask("Image Classifier Server", static_url_path='/Users/piyush/Documents/koala_bear/')
+parser = argparse.ArgumentParser()
+parser.add_argument("--root_dir", required=True, help="Root Directory of Sample Images")
+parser.add_argument("--upload_dir", default='uploads', help="Root Directory of Sample Images")
+args = parser.parse_args()
+
+# @app.route('/')
+# def home():
+#     sample_imgs = glob.glob(os.path.join(args.root_dir, "*.jpg"))
+#     return render_template('index.html', img_paths=sample_imgs)
+
+@app.route('/')
+def home():
+    sample_imgs = os.listdir(args.root_dir)
+    return render_template('index.html', names=sample_imgs)
+
+@app.route('/uploads/<path:filename>')
+def download_file(filename):
+    return send_from_directory(args.root_dir, filename, as_attachment=False)
+
+@app.route('/classify', methods=['POST'])
+def classify():
+    file = request.files['img']
+    file.save(os.path.join(args.upload_dir, file.filename))
+    return 'testResponse'
+
+if __name__=="__main__":
+    app.run(host='0.0.0.0', port=8080, debug=True)
+
+
+# def main():
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument("--root_dir", required=True, help="Root Directory of Sample Images")
+    # args = parser.parse_args()
+    # sample_imgs = glob.glob(os.path.join(args.root_dir, "*.jpg"))
